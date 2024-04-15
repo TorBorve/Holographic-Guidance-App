2bb8750963700d6f628b19818693d74159abf746
Faye Zhang <ffz@seas.upenn.edu>
2024-04-15 20:57:36 +0200

    added working debugger panel that prints msg in scene

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -1,31 +1,42 @@
 using Microsoft.MixedReality.Toolkit.Utilities;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Tutorials
 {
     public class Follower : MonoBehaviour
     {
+        //[SerializeField]
         private Recorder _recorder;
+        //[SerializeField]
         private Player _player;
 
         private bool followMode = false;
         private float timeKeeper = 0f;
 
-        public Follower() { }
+        [SerializeField]
+        private GameObject debugger;
+
+        public Follower() {}
         public void SetFollower(Player player, Recorder recorder)
         {
             _recorder = recorder;
             _player = player;
+            TextMesh txt = debugger.GetComponent<TextMesh>();
+            txt.text = "wtf";
         }
         public void playAnimation()
         {
+            //TextMesh txt = obj.GetComponent<TextMesh>();
+            //txt.text = "PlayAnimation";
             followMode = true;
-            _player.StartFollowAnimation();
-            _recorder.StartRecording();
+            //_player.StartFollowAnimation();
+            //_recorder.StartRecording();
         }
         public void Update()
         {
+            TextMesh txt = debugger.GetComponent<TextMesh>();
             if (timeKeeper == 0)
             {
                 timeKeeper = 2.0f;
@@ -33,25 +44,30 @@ namespace Tutorials
             InputRecordingBuffer.Keyframe key;
             if (followMode)
             {
-                key = _recorder.GetLatestKeyframe();
-                IDictionary<TrackedHandJoint, TransformData>  joints = _player.GetAnimationByTime(timeKeeper);
-                Vector3 john = joints[TrackedHandJoint.Wrist].GetPosition();
-                Vector3 vect = key.RightJoints[TrackedHandJoint.Wrist].Position;
+                //txt.text = "follow mode on";
+                txt.text += timeKeeper.ToString();
+                //key = _recorder.getlatestkeyframe();
+                //idictionary<trackedhandjoint, transformdata> joints = _player.getanimationbytime(timekeeper);
+                //vector3 john = joints[trackedhandjoint.wrist].getposition();
+                //vector3 vect = key.rightjoints[trackedhandjoint.wrist].position;
 
-                float distance = Vector3.Distance(john, vect);
-                Debug.Log(distance);
-                if (timeKeeper == 0)
-                {
-                    timeKeeper = 5.0f;
-                }
-                if (distance < 0.20)
-                {
-                    timeKeeper += 0.1f;
-                }
-                Debug.Log(timeKeeper);
+                //float distance = vector3.distance(john, vect);
+                //debug.log(distance);
+                //if (timekeeper == 0)
+                //{
+                //    timekeeper = 5.0f;
+                //}
+                //if (distance < 0.20)
+                //{
+                //    timekeeper += 0.1f;
+                //}
+                // Debug.Log(timeKeeper);
+            } else
+            {
+                txt.text = "Follow mode off";
             }
-            
-            _player.setLocalTime(timeKeeper);
+
+            //_player.setLocalTime(timeKeeper);
         }
 
     }
